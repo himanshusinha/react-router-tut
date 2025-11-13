2315eb45f009f0652ceec2f461981e03087b1560
hsinha1110 <97324633+hsinha1110@users.noreply.github.com>
2025-11-13 14:10:47 +0530

    404 Error

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { Route, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import Home from "./Home";
 import About from "./About";
 import Login from "./Login";
 import NavBar from "./NavBar";
+import PageNotFound from "./PageNotFound";
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
+        {/* <Route path="/*" element={<PageNotFound />} /> */}
+        <Route path="/*" element={<Navigate to="/login" />} />
       </Routes>
     </>
   );
---
+++ src/PageNotFound.jsx
@@ -0,0 +1,14 @@
+function PageNotFound() {
+  return (
+    <div
+      style={{
+        justifyContent: "center",
+        alignItems: "center",
+        textAlign: "center",
+      }}
+    >
+      <h1>Page Not Found</h1>
+    </div>
+  );
+}
+export default PageNotFound;
