27426683a2ba40e20231632094a0384557752413
hsinha1110 <97324633+hsinha1110@users.noreply.github.com>
2025-11-17 23:58:35 +0530

    Get API

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,26 @@
+import { useEffect, useState } from "react";
+
 export default function App() {
-  return <h1 className="bg-red-700 font-bold text-3xl ">Hello world!</h1>;
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    getUsers();
+  }, []);
+  const getUsers = async () => {
+    const response = await fetch("https://dummyjson.com/users");
+    const data = await response.json();
+    setUsers(data.users);
+  };
+  return (
+    <div>
+      <h2>
+        {users.map((user) => (
+          <div>
+            <h2 key={user.id}>{user.firstName}</h2>
+            <h2 key={user.id}>{user.lastName}</h2>
+            <h2 key={user.id}>{user.email}</h2>
+          </div>
+        ))}
+      </h2>
+    </div>
+  );
 }
